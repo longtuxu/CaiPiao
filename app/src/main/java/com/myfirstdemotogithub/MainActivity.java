52df1c512375cd6450bf1cf6d3b3d03da6c58d47
MarkoLong <2859698459@qq.com>
2018-07-04 16:13:52 +0800

    For test the commit to github.

--- app/src/main/java/com/myfirstdemotogithub/MainActivity.java
+++ app/src/main/java/com/myfirstdemotogithub/MainActivity.java
@@ -2,6 +2,7 @@ package com.myfirstdemotogithub;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity
 {
@@ -11,5 +12,6 @@ public class MainActivity extends AppCompatActivity
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Toast.makeText(this, "hello world", Toast.LENGTH_SHORT).show();
     }
 }
